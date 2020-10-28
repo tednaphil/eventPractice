5cc57b3051ffc61d5c8215c03b5abb224e2dec31
Hannah Hudson <hannah.hch@gmail.com>
2020-10-28 14:09:20 -0600

    Update exercise 3

--- example3/script.js
+++ example3/script.js
@@ -1,13 +1,6 @@
 var addNewButton = document.querySelector('.add-new');
-var parent = document.querySelector('.parent');
 
 addNewButton.addEventListener('click', createButton);
-parent.addEventListener('click', function(event) {
-  if(event.target.className === 'click-me') {
-    // do your action on your 'button' or whatever
-    // it is you're listening for
-  }
-})
 
 function showAlert() {
   alert('You clicked me!');

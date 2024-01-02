4e2495922c975c79616773d5a3f62101436bc95b
Tayla Phillips <richardstayla@gmail.com>
2024-01-02 14:55:36 -0600

    Refactor alert event listener

--- example1/script.js
+++ example1/script.js
@@ -2,16 +2,23 @@ var addNewButton = document.querySelector('.add-new');
 var parent = document.querySelector('.parent');
 var allButtons = document.querySelectorAll('.parent .click-me');
 
+
 addNewButton.addEventListener('click', createButton);
 
-for(var i = 0; i < allButtons.length; i++) {
-  allButtons[i].addEventListener('click', showAlert);
+parent.addEventListener('click', showAlert)
+
+// for(var i = 0; i < allButtons.length; i++) {
+//   allButtons[i].addEventListener('click', showAlert);
+// }
+
+function showAlert(e) {
+  // console.log(e.target)
+  if (e.target.className === 'click-me') {
+    alert(`You clicked me!`)
+  }
 }
 
-function showAlert() {
-  alert('You clicked me!');
-}
-
+// what edge cases are there for inclusion in function vs event listener?
 function createButton() {
   var newButton = document.createElement('button');
   newButton.className = 'click-me';

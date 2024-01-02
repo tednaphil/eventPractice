70f732422f7517b5671abe4ed61f731b6a8e15ae
Tayla Phillips <richardstayla@gmail.com>
2024-01-02 14:56:26 -0600

    Add event.target console logs

--- example2/script.js
+++ example2/script.js
@@ -1,3 +1,17 @@
 var grandParent = document.querySelector('.grandparent');
 var parent = document.querySelector('.parent');
 var button = document.querySelector('#click-me');
+
+grandParent.addEventListener('click', function(e) {
+    console.log(`event.target:`, e.target);
+    console.log(`grandparent properties:`, e)
+    // console.log('Grandparent');
+});
+
+// parent.addEventListener('click', function(e) {
+//     console.log(`parent properties:`, e);
+// });
+
+// button.addEventListener('click', function(e) {
+//     console.log(`button properties:`, e);
+// });
